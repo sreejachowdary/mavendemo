ea11f5fc59667e88634820622b06dda7cf224667
sreeja <srija.chow512@gmail.com>
2019-06-06 20:43:03 +0530

    maven is runn

---
+++ src/main/java/maveen1/mvn1.java
@@ -0,0 +1,26 @@
+package maveen1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class mvn1 {
+	@Test
+public void Test1(){
+		
+		WebDriverManager.chromedriver().setup();
+		//System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.seleniumhq.org");
+		System.out.println(driver.getTitle());
+		driver.findElement(By.xpath("//a[@title='Get Selenium']")).click();
+		System.out.println(driver.getTitle());
+		driver.close();
+	}
+
+}
+
+
