5e066853c7a6b36e369c4f05b8e8becd28f346de
sreeja <srija.chow512@gmail.com>
2019-06-07 10:56:06 +0530

    changes made

--- src/main/java/maveen1/mvn1.java
+++ src/main/java/maveen1/mvn1.java
@@ -17,7 +17,8 @@ public void Test1(){
 		driver.get("https://www.seleniumhq.org");
 		System.out.println(driver.getTitle());
 		driver.findElement(By.xpath("//a[@title='Get Selenium']")).click();
-		//System.out.println(driver.getTitle());
+		System.out.println(driver.getTitle());
+		System.out.println("okay");
 		driver.close();
 	}
 
